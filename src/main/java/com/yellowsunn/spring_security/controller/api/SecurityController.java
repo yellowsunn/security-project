0f9b551c39eef6fed17d3c7818b50b51251dedff
yellowsunn <chohankook95@gmail.com>
2020-12-22 18:09:47 +0900

    /api 접근 시 UserDto 객체반환

--- src/main/java/com/yellowsunn/spring_security/controller/api/SecurityController.java
+++ src/main/java/com/yellowsunn/spring_security/controller/api/SecurityController.java
@@ -1,26 +1,74 @@
 package com.yellowsunn.spring_security.controller.api;
 
+import com.yellowsunn.spring_security.domain.dto.UserDto;
+import com.yellowsunn.spring_security.domain.entity.Account;
+import com.yellowsunn.spring_security.security.CustomUserDetails;
+import org.springframework.security.authentication.RememberMeAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class SecurityController {
 
     @GetMapping("")
-    public String main() {
-        return "Authorized";
+    public UserDto main() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDto.UserDtoBuilder userDtoBuilder = UserDto.builder();
+
+        setUsername(authentication, userDtoBuilder);
+        setAuthority(authentication, userDtoBuilder);
+
+        return userDtoBuilder.build();
     }
 
     @GetMapping("/user")
     public String user() {
         return "user";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    private void setUsername(Authentication authentication, UserDto.UserDtoBuilder userDtoBuilder) {
+        // 일반 인증인 경우
+        if (authentication instanceof UsernamePasswordAuthenticationToken) {
+            Account account = (Account) authentication.getPrincipal();
+            userDtoBuilder.username(account.getUsername());
+
+            // Remember Me 인증인 경우
+        } else if (authentication instanceof RememberMeAuthenticationToken) {
+            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+            userDtoBuilder.username(customUserDetails.getUsername());
+        }
+    }
+
+    private void setAuthority(Authentication authentication, UserDto.UserDtoBuilder userDtoBuilder) {
+        Set<String> authorities = authentication.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet());
+
+        if (authorities.contains("ROLE_ADMIN")) {
+            userDtoBuilder.role("ADMIN");
+        } else if (authorities.contains("ROLE_MANAGER")) {
+            userDtoBuilder.role("MANAGER");
+        } else if (authorities.contains("ROLE_USER")) {
+            userDtoBuilder.role("USER");
+        }
     }
 }
--- src/main/java/com/yellowsunn/spring_security/domain/dto/UserDto.java
+++ src/main/java/com/yellowsunn/spring_security/domain/dto/UserDto.java
@@ -1,11 +1,20 @@
 package com.yellowsunn.spring_security.domain.dto;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
 
-@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter @Setter
+@Builder
+@ToString
 public class UserDto {
 
     private String username;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String password;
     private String role;
 }
+
